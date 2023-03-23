5387405624c853a6477e84aa6872cbc01938e913
NAdia <Nadia.m.mirzai@gmail.com>
2023-03-23 14:19:03 -0400

    changed to True

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import pytest
 
 
 def always_returns_true():
-    # this time i added a comment
-    x = None
-    return x 
+    # it should have been True
+    y = True
+    return y
 
 
 def test_always_returns_true():
+
+    
+    # No changes here
     assert always_returns_true()
+ 
